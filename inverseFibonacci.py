3f22c717ac5ed6e85ec563907b394bf21a302f26
Johan Air <johan@nexys.ch>
2016-07-03 12:14:56 +0200

    added mandelbrot

---
+++ inverseFibonacci.py
@@ -0,0 +1,15 @@
+"""
+  Reciprocal Fibonacci constant
+  script that computes the golden ratio form the sum of the reciprocals of the Fibonacci numbers
+  @see https://en.wikipedia.org/wiki/Reciprocal_Fibonacci_constant
+"""
+x = 1
+p1 = 1
+p2 = 1
+
+for i in range(1,50):
+	x += 1.0/p2
+	p2 += p1
+	p1 = p2 - p1
+
+	print x
