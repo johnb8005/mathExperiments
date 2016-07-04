2f6fc4982bb1025a96c636820829cc7eca954b29
Johan Air <johan@nexys.ch>
2016-07-04 15:41:46 +0200

    koch snoflwake

--- fractals/kochSowflake.py
+++ fractals/kochSowflake.py
@@ -14,17 +14,6 @@ def addLine(c1, c2):
   svg.add(svg.line(c1, c2, stroke=svgwrite.rgb(10, 10, 16, '%')))
 
 
-def middlePoint(c1, c2):
-  return ptRatiofSegment(c1, c2, .5)
-
-def ptRatiofSegment(c1, c2, a):
-  x = a*(c2[0] - c1[0]) + c1[0]
-  y = a*(c2[1] - c1[1]) + c2[1]
-  return (x,y)
-
-def slope(c1, c2):
-  return (c2[1] - c1[1])/(c2[0] - c1[0])
-
 def addVectorToPoint(pt, v):
   return (pt[0]+v[0], pt[1]+v[1])
 
@@ -39,40 +28,46 @@ def addPolarVectortoPoint(pt, v):
   return addVectorToPoint(pt, (x,y))
 
 
-def calculateH(l):
-  return l/np.sqrt(12)
+
+def find4NextPts(pt, l ,alpha):
+  pt1 = addPolarVectortoPoint(pt, (l/3,alpha))
+  pt2 = addPolarVectortoPoint(pt1,(l/3, alpha-np.pi/3)) 
+  pt3 = addPolarVectortoPoint(pt, (2*l/3,alpha))
+
+  pt4 = addPolarVectortoPoint(pt, (l, alpha))
+
+  return (pt1, pt2, pt3, pt4)
 
 
-print np.sin(np.pi/3)
-print np.sqrt(3)/2
+# length of triangle side
+l = 210
 
+# inital point
+pt1 = (100,100)
 
-l = 180
-pt1 = (20,80)
-pt2 = (l+pt1[0], pt1[1])
-pt3 = addPolarVectortoPoint(pt2, (180, np.pi/2+np.pi/6))
+# initial angle
+alpha = 0
 
+# depth of complexity
+depth = 2
+# number of points
+n = 3*(4**(depth-1))
+pts = np.empty([n,2])
 
-addLine(pt2, pt3)
-addLine(pt3, pt1)
+# angle increment when going to a new side (do not change)
+alphainc = np.pi/2+np.pi/6
 
-
+ppt = pt1
+for i in range(0,3):
 # find 1/3, 1/2 and 2/3 of section
-pt11 = addPolarVectortoPoint(pt1, (l/3,0))#  (pt1[0]+l/3, pt1[1]) #ptRatiofSegment(pt1, pt2, 1/3)
-pt13 = addPolarVectortoPoint(pt1, (2*l/3,0)) #(pt1[0]+2*l/3, pt1[1])
+  alpha = i*alphainc
+  pts[i*4:(i*4+4),:] = find4NextPts(ppt, l, alpha)
+  ppt = pts[i*4+3]
 
-h = -calculateH(l)
-print h
-pt14 = addPolarVectortoPoint(pt11, (l/3, -np.pi/3))
-
-addLine(pt1,  pt11)
-addLine(pt11, pt14)
-addLine(pt14, pt13)
-addLine(pt13, pt2)
-
-
-#pt6 = addPolarVectortoPoint(pt5, (180, np.pi+np.pi/3))
-
+ppt = pt1
+for pt in pts:
+  addLine(ppt, pt)
+  ppt = pt
 
 
 
